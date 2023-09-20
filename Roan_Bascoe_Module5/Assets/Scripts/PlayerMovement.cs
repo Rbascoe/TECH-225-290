24acbc28eaadc607b1f140f73335e728391d7e18
Rbascoe <roanbascoe5@gmail.com>
2023-09-20 10:21:34 -0400

    finished Module 6

--- Roan_Bascoe_Module5/Assets/Scripts/PlayerMovement.cs
+++ Roan_Bascoe_Module5/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float playerSpeed = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer("Vertical");
+        movePlayer("Horizontal");
     }
+
+    void movePlayer(string direction)
+    {
+
+        Vector3 moveDirection;
+        float directionalInput = Input.GetAxis(direction);
+
+        switch (direction)
+        {
+            case "Vertical":
+                moveDirection = transform.forward * directionalInput;
+                break;
+            case "Horizontal":
+                moveDirection = transform.right * directionalInput;
+                break;
+            case "Turn Right":
+                if (Input.GetKey(KeyCode.M))
+                {
+                    transform.Rotate(Vector3.forward * playerSpeed * Time.deltaTime);
+
+                }
+                break;
+            case "Turn Left":
+                if (Input.GetKey(KeyCode.N))
+                {
+                    transform.Rotate(Vector3.forward * playerSpeed * Time.deltaTime);
+
+                }
+                break;
+            default:
+                Debug.Log("");
+                break;
+
+        }
+
+        // Input.GetKey(KeyCode.M)
+        // if (direction == "Vertical")
+        // {
+        //     moveDirection = transform.forward * directionalInput;
+
+        // }
+        // else if(direction == "Vertical")
+        // {
+        //     moveDirection = transform.right * directionalInput;
+
+        // }
+
+        // transform.Translate(moveDirection * playerSpeed * Time.deltaTime);
+        //transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+
+    void movePlayerBack()
+    {
+
+    }
+
+    void turnPlayer()
+    {
+
+    }
+
 }
---
+++ Roan_Bascoe_Module6/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    private Renderer playerColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        playerColor = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            changeColor();
+        }
+
+    }
+
+    void changeColor()
+    {
+        Color randomColor = new Color(Random.value, Random.value, Random.value);
+
+        playerColor.material.color = randomColor;
+    }
+
+
+}
